64cc5e4bcafe37ea9477f3a207b2c9b1c614fb3e
Kris Quigley <5502215+krisquigley@users.noreply.github.com>
2025-11-05 17:27:55 +0000

    fix: ssl redirect

--- config/prod.exs
+++ config/prod.exs
@@ -7,7 +7,7 @@ import Config
 # before starting your production server.
 config :jarga, JargaWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
-  force_ssl: [hsts: true]
+  force_ssl: [hsts: true, rewrite_on: [:x_forwarded_proto]]
 
 # Configures Swoosh API Client
 config :swoosh, api_client: Swoosh.ApiClient.Req
