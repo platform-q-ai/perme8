17b38e29ef92de5dcd2334ba3fbc0e8e22c3ac0c
Kris Quigley <5502215+krisquigley@users.noreply.github.com>
2025-11-07 18:25:35 +0000

    fix: toast appear behind modal

--- lib/jarga_web/components/core_components.ex
+++ lib/jarga_web/components/core_components.ex
@@ -58,7 +58,7 @@ defmodule JargaWeb.CoreComponents do
       phx-hook="AutoHideFlash"
       phx-click={JS.push("lv:clear-flash", value: %{key: @kind}) |> hide("##{@id}")}
       role="alert"
-      class="toast toast-top toast-end z-50"
+      class="toast toast-top toast-end z-[9999]"
       {@rest}
     >
       <div class={[
