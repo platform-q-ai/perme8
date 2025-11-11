9e4910ef919e2a4a325ca14723bf4e67fd548942
Kris Quigley <5502215+krisquigley@users.noreply.github.com>
2025-11-11 11:26:26 +0000

    fix: email link hostname

--- config/runtime.exs
+++ config/runtime.exs
@@ -69,7 +69,7 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("RENDER_EXTERNAL_HOSTNAME") || System.get_env("PHX_HOST") || "localhost"
+  host = System.get_env("PHX_HOST") || "localhost"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :jarga, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
