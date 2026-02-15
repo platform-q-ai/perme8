d143676615cad8a617bdb53247eb16029021f2a7
Kris Quigley <5502215+krisquigley@users.noreply.github.com>
2026-02-15 22:38:37 +0000

    fix: restore Wallaby deps for ExUnit browser tests in jarga_web

--- apps/jarga_web/test/test_helper.exs
+++ apps/jarga_web/test/test_helper.exs
@@ -1,5 +1,9 @@
+# Start Wallaby for ExUnit browser tests (gfm_checkbox, undo_redo, etc.)
+# BDD features are now run externally via exo-bdd (mix exo_test --app jarga_web)
+{:ok, _} = Application.ensure_all_started(:wallaby)
+Application.put_env(:wallaby, :base_url, JargaWeb.Endpoint.url())
+
 # Exclude evaluation tests, browser-based tests, and WIP features by default
-# Browser BDD features are now run externally via exo-bdd (mix exo_test --app jarga_web)
 ExUnit.start(exclude: [:evaluation, :javascript, :wip], capture_log: true)
 
 Ecto.Adapters.SQL.Sandbox.mode(Jarga.Repo, :manual)
