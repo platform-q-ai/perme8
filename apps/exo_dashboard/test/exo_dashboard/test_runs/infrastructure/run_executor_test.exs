1981de8a4270b4cf52d68e77cfae21df1ba2d9e2
Kris Quigley <5502215+krisquigley@users.noreply.github.com>
2026-02-20 13:45:27 +0000

    test: address review - add start/2 test coverage for RunExecutor public API

--- apps/exo_dashboard/test/exo_dashboard/test_runs/infrastructure/run_executor_test.exs
+++ apps/exo_dashboard/test/exo_dashboard/test_runs/infrastructure/run_executor_test.exs
@@ -3,6 +3,49 @@ defmodule ExoDashboard.TestRuns.Infrastructure.RunExecutorTest do
 
   alias ExoDashboard.TestRuns.Infrastructure.RunExecutor
 
+  defmodule MockFileSystem do
+    def mkdir_p!(_path), do: :ok
+  end
+
+  defmodule TrackingFileSystem do
+    def mkdir_p!(path) do
+      send(:run_executor_test, {:mkdir_p!, path})
+      :ok
+    end
+  end
+
+  describe "start/2" do
+    @tag :external
+    test "spawns a task and returns {:ok, pid}" do
+      {:ok, pid} =
+        RunExecutor.start("test-run-1",
+          scope: {:app, "jarga_web"},
+          file_system: MockFileSystem
+        )
+
+      assert is_pid(pid)
+      assert Process.alive?(pid)
+    end
+
+    test "calls mkdir_p! on the file_system module" do
+      Process.register(self(), :run_executor_test)
+
+      {:ok, _pid} =
+        RunExecutor.start("test-run-2",
+          scope: {:app, "jarga_web"},
+          file_system: TrackingFileSystem
+        )
+
+      assert_receive {:mkdir_p!, "/tmp/exo_dashboard"}, 2000
+    after
+      try do
+        Process.unregister(:run_executor_test)
+      rescue
+        _ -> :ok
+      end
+    end
+  end
+
   describe "build_args/2" do
     test "builds correct bun args for app scope" do
       args = RunExecutor.build_args("run-1", scope: {:app, "jarga_web"})
